1a3814035874f08af1178352d320fe495fa4d400
Rundmus <mghong@kth.se>
2020-05-08 16:08:56 +0200

    Create `dot_Rmd_template.Rmd`

--- R/use.R_template.R
+++ R/use.R_template.R
@@ -1,14 +1,53 @@
+use.template <- function(file, 
+                         author,
+                         extension,
+                         template_file
+) {
+  stopifnot(
+    !missing(file), 
+    !missing(author)
+  )
+  
+  #  template_path = path to the template file
+  template_path <- system.file(
+    "templates", "template_file", 
+    package= "Useful2me", 
+    mustWork= TRUE
+  )
+  
+  ext_pattern <- paste0("\\", extension, "$")
+  title <- sub(ext_pattern, "", basename(file)) 
+  date <- Sys.Date()
+  
+  ##  Substitute the entities in the templates with the given values in this
+  ##  function
+  out_text <- readLines(template_path) %>% 
+    gsub('\\{\\{\\{ title \\}\\}\\}', title, .) %>% 
+    gsub('\\{\\{\\{ author \\}\\}\\}', author, .) %>% 
+    gsub('\\{\\{\\{ date \\}\\}\\}', as.character(date), .) 
+  
+  ##  allow both with or without .R in 'file'
+  base_fn <- sub(ext_pattern, "", basename(file))
+  path <- file.path(dirname(file), paste0(base_fn, extension))
+  
+  #  **  Write the text from the template after modification  **  #
+  writeLines(out_text, path)
+  
+  message('# The file \"', paste0(base_fn, extension), '\" has been created.')
+  
+  invisible(TRUE)
+}
+
+
+
 # -----------------------------------------------------------------------------#
-#' Use .R template
+#' Use .R[md] template
 #' 
-#' Create .R file using a template that includes header lines
+#' Create .R or .Rmd file using a template that includes header lines
 #' 
-#' @param file The name of the generating .R file. The ".R" extension will be
-#'   added if it is missing.
+#' @param file The name of the generating file. The ".R" or ".Rmd" extension
+#'   will be added if it is missing.
 #' @param author The author of the script
-#' @param title The title of the generating R script, which is displayed at the
-#'   top
-#' @param date The date written as the creation date
 #' 
 #' @examples
 #' \dontrun{
@@ -17,42 +56,47 @@
 #' 
 #' @author Mun-Gwan Hong, \email{mungwan@gmail.com}
 #' @export
+#' 
+#' @rdname use_template
 # -----------------------------------------------------------------------------#
 # created  : 2017-03-02 by Mun-Gwan
 # modified : 
 # -----------------------------------------------------------------------------#
 
-use.R_template <- function(file, 
-                           author, 
-                           title= sub("\\.R$", "", basename(file)), 
-                           date= Sys.Date()
-) {
-  stopifnot(
-    !missing(file), 
-    !missing(author),
-    is.character(title)
+use.R_template <- function(file, author) {
+  use.template(
+    file = file,
+    author = author,
+    extension = ".R",
+    template_file= "dot_R_template.R"
   )
-  
-  #  template_path = path to the template file
-  template_path <- system.file("templates", "dot_R_template.R", 
-                               package= "Useful2me", 
-                               mustWork= TRUE)
-  
-  ##  Substitute the entities in the templates with the given values in this
-  ##  function
-  out_text <- readLines(template_path) %>% 
-    gsub('\\{\\{\\{ title \\}\\}\\}', title, .) %>% 
-    gsub('\\{\\{\\{ author \\}\\}\\}', author, .) %>% 
-    gsub('\\{\\{\\{ date \\}\\}\\}', as.character(date), .) 
-
-  ##  allow both with or without .R in 'file'
-  base_fn <- sub("\\.R$", "", basename(file))
-  path <- file.path(dirname(file), paste0(base_fn, ".R"))
-  
-  #  **  Write the text from the template after modification  **  #
-  writeLines(out_text, path)
-  
-  message('# The file \"', paste0(base_fn, ".R"), '\" has been created.')
-  
-  invisible(TRUE)
 }
+
+
+
+# -----------------------------------------------------------------------------#
+#' @examples
+#' \dontrun{
+#' use.Rmd_template("process_data") 
+#' }
+#' 
+#' @export
+#' 
+#' @rdname use_template
+# -----------------------------------------------------------------------------#
+# created  : 2020-05-08 by Mun-Gwan
+# modified : 
+# -----------------------------------------------------------------------------#
+
+use.Rmd_template <- function(file, author) {
+  use.template(
+    file = file,
+    author = author,
+    extension = ".Rmd",
+    template_file= "dot_Rmd_template.R"
+  )
+}
+
+
+
+
