610bf00c1e7e1c793191f165af4e7ecc8147929b
Rundmus <mungwan@gmail.com>
2020-05-12 14:42:26 +0200

    Add 'tidyverse' as an optional choice of imported package

--- inst/templates/dot_R_template.R
+++ inst/templates/dot_R_template.R
@@ -7,6 +7,7 @@
 rm(list = ls())
 
 library(dplyr)
+# library(tidyverse)
 # library(Useful2me) # remotes::install_github("Rundmus/Useful2me-R_package", upgrade= "never")
 
 #----- File names --------------------------------------------------------------
