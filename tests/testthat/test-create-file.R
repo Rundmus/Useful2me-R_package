695d7100acad5fc3c85b78819d8c530e20bea464
Rundmus <mungwan@gmail.com>
2020-11-12 13:55:34 +0100

    Fix the bug remained for old files

--- tests/testthat/test-create-file.R
+++ tests/testthat/test-create-file.R
@@ -11,20 +11,4 @@ test_that("use.R_template creates file", {
   )
   expect_true(file.exists(paste0(tF, ".R")))
   
-  #  ref: https://stackoverflow.com/questions/41372146/test-interaction-with-users-in-r-package
-  f <- file()
-  options(mypkg.connection = f)
-  ans <- paste(c("n", "y"), collapse = "\n") # set this to the number of tests you want to run
-  write(ans, f)
-  
-  #  'n'
-  expect_message(
-    suppressMessages(use.R_template(tF, " ")), 
-    '# No change.'
-  )
-  #  'y'
-  expect_message(
-    suppressMessages(use.R_template(tF, " ")), 
-    paste0('# The file \"', tF, '.R\" has been created.')
-  )
 })
